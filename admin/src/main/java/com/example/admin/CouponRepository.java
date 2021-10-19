d1c2a2a42144d34f33be987d5543b47d61d0f3c6
antonychirayil <antonychirayil@users.noreply.github.com>
2021-10-19 15:30:04 +0530

    Eureka added

--- admin/src/main/java/com/example/admin/CouponRepository.java
+++ admin/src/main/java/com/example/admin/CouponRepository.java
@@ -3,6 +3,5 @@ package com.example.admin;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface CouponRepository extends MongoRepository<Coupon, String>{
-
 	Coupon findProductById(String ID);
 	}
